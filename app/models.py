7e28c92d0008a54d99221c6b5ca2a18c4482b842
danielluccio <daniel130877@gmail.com>
2025-07-17 13:43:45 -0300

    modelos de Categoria e Tarefa feitos

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=200)
+
+
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=250)
+    descricao = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    inclusao = models.DateTimeField(auto_now_add=True)
+    concluida = models.BooleanField()
+
+    
